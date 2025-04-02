21d51812d1889650d8ad83033bb63e38b1f72fb3
Nico <nicolasfersou@gmail.com>
2025-04-02 20:40:02 +0200

    First commit

---
+++ src/main/java/com/beatstore/BeatStore/BeatStoreApplication.java
@@ -0,0 +1,13 @@
+package com.beatstore.BeatStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beatstore/BeatStore/BeatStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beatstore.BeatStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
